907e56f69d40d6cc111461eca4167707e68a4ae9
Богдан Семенюк <>
2023-09-14 11:46:30 +0300

    project init

---
+++ Movie Explorer/Movie_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ExplorerApp.swift
+//  Movie Explorer
+//
+//  Created by Богдан Семенюк on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
