3610432b4b51eee8a87607ee4df350ed4bd7638e
Богдан Семенюк <>
2023-09-26 12:26:42 +0300

    add SignUpView

---
+++ Movie Explorer/Modules/SignUpView.swift
@@ -0,0 +1,22 @@
+//
+//  SignUpView.swift
+//  Movie Explorer
+//
+//  Created by Богдан Семенюк on 26.09.2023.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        VStack {
+            HeaderWelcomeView(text: String(localized: "SignInView.welcomeText"))
+        }
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
